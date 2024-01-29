54d980e1fa2a8282daef888fbf321dfa5e9e0594
eyal-mor <9406299+eyal-mor@users.noreply.github.com>
2024-01-29 14:38:50 +0200

    Expire all images except the last 2 as a cost saving measure.

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -7,3 +7,26 @@ resource "aws_ecr_repository" "repo" {
   }
 }
 
+
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = aws_ecr_repository.foo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only the last 2 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
